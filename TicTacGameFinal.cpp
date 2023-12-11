4857fd13b4268bb2911c49ccda3dbdbae6ec6fbe
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-12-11 10:04:48 +0530

    Add files via upload

---
+++ TicTacGameFinal.cpp
@@ -0,0 +1,126 @@
+// TICK TAC TOE GAME
+
+#include <iostream>
+using namespace std;
+char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+char turn = 'x';
+int row, column;
+void display_board()
+{
+    system("cls");
+
+    cout << "\n\n    TICK CROSS GAME " << endl; //  Here back slice t means space tab for space
+    cout << "\tPlayer1 [x] \n\tplayer2 [O]\n\n";
+
+    cout << "\t\t  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "  \n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t_____|_____|_____\n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "  \n";
+    cout << "\t\t_____|_____|_____\n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << "  \n";
+}
+void player_turn()
+{
+    int choice;
+    if (turn == 'x')
+
+        cout << "\n\tplayer1 [x] turn:";
+    if (turn == 'O')
+
+        cout << "\n\tplayer2 [O] turn:";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        row = 0;
+        column = 0;
+        break;
+    case 2:
+        row = 0;
+        column = 1;
+        break;
+    case 3:
+        row = 0;
+        column = 2;
+        break;
+    case 4:
+        row = 1;
+        column = 0;
+        break;
+    case 5:
+        row = 1;
+        column = 1;
+        break;
+    case 6:
+        row = 1;
+        column = 2;
+        break;
+    case 7:
+        row = 2;
+        column = 0;
+        break;
+    case 8:
+        row = 2;
+        column = 1;
+        break;
+    case 9:
+        row = 2;
+        column = 2;
+        break;
+
+    default:
+        cout << "Invalid choice \n";
+        break;
+    }
+    if (turn == 'x' && board[row][column] != 'x' && board[row][column] != 'O')
+
+    {
+        board[row][column] = 'x';
+        turn = 'O';
+    }
+
+    else if (turn == 'O' && board[row][column] != 'x' && board[row][column] != 'O')
+    {
+        board[row][column] = 'O';
+        turn = 'x';
+    }
+    else
+    {
+        cout << " Box already filled!\n please try again!! \n\n";
+        player_turn();
+    }
+    display_board();
+}
+bool gameover()
+{
+    // check who win
+    for (int i = 0; i < 3; i++)
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            return false;
+
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[1][1] && board[0][0] == board[2][0])
+        return false;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'x' && board[i][j] != 'O')
+                return true;
+}
+
+int main()
+{
+    while (gameover())
+    {
+
+        display_board();
+        player_turn();
+        gameover();
+    }
+    if (turn == 'x')
+        cout << "player2 [O] wins!! congratulations\n";
+    if (turn == 'O')
+        cout << "player1 [x] wins!! congratulations\n";
+
+    return 0;
+}
